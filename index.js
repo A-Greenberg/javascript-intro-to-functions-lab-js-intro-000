a9966dbd5aad8dbf27f650d5f0dd0d4e411ebf78
Alon G <greenberg.alon@gmail.com>
2020-09-08 21:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 
 //eceives one argument and returns it in all caps
-
 function shout (string) {
   return string.toUpperCase();
 }
 
 // receives one argument and returns it in all lowercase
-
 function whisper (string) {
   return string.toLowerCase();
 }
 
 //calls console.log() its one argument in all caps
-
 function logShout(string) {
 
 }
 
 //calls console.log() its one argument in all lowercase
-
 function logWhisper(string) {
 
 }
@@ -27,8 +23,6 @@ function logWhisper(string) {
 //  5) returns "I can't hear you!" if `string` is lowercase
 //  6) returns "YES INDEED!" if `string` is uppercase
 //  7) returns "I love you, too." if `string` is "I love you, Grandma."`
-
-
 function sayHiToGrandma(string) {
 
 }
