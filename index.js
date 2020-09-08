d13ea668da646e1a122b53ee258a2166266b98ee
Alon G <greenberg.alon@gmail.com>
2020-09-08 21:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 //eceives one argument and returns it in all caps
 
 function shout (string) {
- return string.toUpperCase();
+  return string.toUpperCase();
 }
 
 // receives one argument and returns it in all lowercase
 
 function whisper (string) {
-
+  return string.toLowerCase();
 }
 
 //calls console.log() its one argument in all caps
